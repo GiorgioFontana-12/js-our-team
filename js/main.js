9f6a2f09084cd8d5136e36a774d88044cb92cdaf
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-02 15:59:09 +0200

    feat obgect array

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const team = [
+    {name: "Wayne Barnett", work: "Founder & CEO",      	img: "wayne-barnett-founder-ceo.jpg"        },
+    {name: "Angela Caroll", work: "Chief Editor",	        img: "angela-caroll-chief-editor.jpg"       },
+    {name: "Walter Gordon", work: "Office Manager",      	img: "walter-gordon-office-manager.jpg"     },
+    {name: "Angela Lopez",  work: "Social Meedia Manager",	img: "angela-lopez-social-media-manager.jpg"},
+    {name: "Scott Estrada", work: "Developer",	            img: "scott-estrada-developer.jpg"          },
+    {name: "Barbara Ramos", work: "Graphic Designer",       img: "barbara-ramos-graphic-designer.jpg"   }
+];
+
+console.log(team)
+
+for (const key in team) {
+    if (Object.hasOwnProperty.call(team, key)) {
+        const teamComponent = team[key];
+        console.log(teamComponent)
+    }
+}
